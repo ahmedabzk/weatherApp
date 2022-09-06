b0e86a1eb8beb913fbe054d8bdd046de82ad274b
ahmedabzk <ahmed.abzk@gmail.com>
2022-09-06 17:26:04 +0300

    weather app

--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"bytes"
+	// "bytes"
+	// "bytes"
 	"encoding/json"
-	"fmt"
+	// "fmt"
 	"io/ioutil"
+	"net/http"
+	"strings"
 )
 type apiConfigData struct{
-	OpenWeatherMapApiKey string `json:"OpenWeatherMapApiKey`
+	OpenWeatherMapApiKey string `json:"OpenWeatherMapApiKey"`
 }
 type weatherData struct{
-	Name string `json:"name`
+	Name string `json:"name"`
 	Main struct{
-		kelvin float64 `json:"temp`
-	}`json:"main`
+		Kelvin float64 `json:"temp"`
+	}`json:"main"`
 }
 func loadApiKey(filename string)(apiConfigData, error){
 	bytes, err := ioutil.ReadFile(filename)
@@ -30,4 +33,44 @@ func loadApiKey(filename string)(apiConfigData, error){
 	}
 
 	return c, nil
+}
+func hello(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("hellooo from meee"))
+	// w.Header().Set("Content-Type","application/json")
+	// json.NewEncoder(w).Encode("Helloo")
+}
+func query(city string)(weatherData, error){
+	apiConfig, err := loadApiKey(".apiConfig")
+	if err != nil{
+		return weatherData{}, err
+	}
+	resp, err := http.Get("https://api.openweathermap.org/data/2.5/weather?APPID=" + apiConfig.OpenWeatherMapApiKey + "&q=" + city)
+	if err != nil {
+		return weatherData{}, err
+	}
+
+	defer resp.Body.Close()
+
+	var d weatherData
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil{
+		return weatherData{}, err
+	}
+	return d, nil
+}
+
+func main(){
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/weather/", 
+	func(w http.ResponseWriter, r *http.Request){
+		city:= strings.SplitN(r.URL.Path, "/",3)[2]
+		data, err := query(city)
+		if err != nil{
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type","application/json: charset=utf-8")
+		json.NewEncoder(w).Encode(data)
+	})
+
+	http.ListenAndServe(":3000",nil)
 }
