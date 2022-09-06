2ff14c4bbf3d7be57fffcfdf7776921d488d90cf
ahmedabzk <ahmed.abzk@gmail.com>
2022-09-06 15:28:55 +0300

    weather app

--- main.go
+++ main.go
@@ -1,6 +1,33 @@
 package main
-import(
-	"fmt"
-)
 
-func main(){}
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+type apiConfigData struct{
+	OpenWeatherMapApiKey string `json:"OpenWeatherMapApiKey`
+}
+type weatherData struct{
+	Name string `json:"name`
+	Main struct{
+		kelvin float64 `json:"temp`
+	}`json:"main`
+}
+func loadApiKey(filename string)(apiConfigData, error){
+	bytes, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		return apiConfigData{}, err
+	}
+
+	var c apiConfigData
+
+	err = json.Unmarshal(bytes, &c)
+	if err != nil{
+		return apiConfigData{}, err
+	}
+
+	return c, nil
+}
